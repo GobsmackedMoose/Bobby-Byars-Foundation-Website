1bfba8917789e7feae6d45f6ac2b3535bd7e8e4f
Sarah Peeralam <speeralam@gmail.com>
2026-02-19 16:38:13 -0600

    ionic

---
+++ Bobbybyars/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Bobbybyars',
+  webDir: 'www'
+};
+
+export default config;
