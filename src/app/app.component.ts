5e24699b9bb9923ed4f867e57ed30d0346c1a92e
Sarah Peeralam <speeralam@gmail.com>
2026-03-12 15:14:05 -0500

    works?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
+    { title: 'Events', url: '/folder/inbox', icon: 'mail' },
+    { title: 'Calendar', url: '/folder/outbox', icon: 'paper-plane' },
+    { title: 'News', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Programs', url: '/folder/archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
