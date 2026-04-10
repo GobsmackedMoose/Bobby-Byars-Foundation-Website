afa8c2b5152d1733740a45d7034a81eab2ba6593
Ryland Crowder <rylandcrowder@gmail.com>
2026-04-10 15:17:55 -0500

    made popup work

--- BobbyByarsWebsite/src/app/app.component.ts
+++ BobbyByarsWebsite/src/app/app.component.ts
@@ -12,9 +12,10 @@ export class AppComponent {
   constructor(private popoverController: PopoverController) {}
   
   
-  async openProgramsMenu() {
+  async openProgramsMenu(event: Event) {
     const popover = await this.popoverController.create({
       component: ProgramsPopoverComponent,
+      event: event,
       translucent: true,
     });
     await popover.present();
