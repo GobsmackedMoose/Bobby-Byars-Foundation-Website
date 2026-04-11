8c9e8c7c266006ce21c69a5d689787ce98f60421
Ryland Crowder <rylandcrowder@gmail.com>
2026-04-11 16:44:59 -0500

    working on navigation system

--- BobbyByarsWebsite/src/app/news-article/news-article.page.ts
+++ BobbyByarsWebsite/src/app/news-article/news-article.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../models/article';
-//import { Firebase }
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-news-article',
@@ -10,29 +10,34 @@ import { Article } from '../models/article';
 })
 export class NewsArticlePage implements OnInit {
 
-  testArticle: Article = new Article(
+  allArticlesTest: Article[] = [new Article(
     "Test Article Title",
     "Test Article Subtitle",
     "01/01/2024",
     "This is the content of the test article. It is meant to demonstrate how an article will be displayed on the news article page.",
     "https://via.placeholder.com/150",
     "Placeholder image for test article"
-  );
+  ),
 
-  testArticle2: Article = new Article(
+  new Article(
     "Test Article number 2",
     "Test Article Subtitle number 2",
     "01/01/2026",
     "This is the content of the second test article. It is meant to demonstrate how an article will be displayed on the news article page.",
     "https://via.placeholder.com/150",
     "Placeholder image for second test article"
-  );
+  )];
 
-  constructor() {
+  allArticles: Article[] = this.allArticlesTest;
+  article: Article | undefined;
+
+  constructor(private route: ActivatedRoute) {
     
   }
 
   ngOnInit() {
+    const slug = this.route.snapshot.paramMap.get('articleId');
+    this.article = this.allArticles.find(article => article.slug === slug);
   }
 
 }
