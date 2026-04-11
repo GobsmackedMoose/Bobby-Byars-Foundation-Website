65a88488d0ffad57b9a8fde17481cae6b6712ca1
Ryland Crowder <rylandcrowder@gmail.com>
2026-04-11 15:30:39 -0500

    trying some stuff for the articles

--- BobbyByarsWebsite/src/app/events/events-routing.module.ts
+++ BobbyByarsWebsite/src/app/events/events-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { EventsPage } from './events.page';
+import { NewsArticlePage } from '../news-article/news-article.page';
 
 const routes: Routes = [
   {
     path: '',
     component: EventsPage
+  },
+  {
+    path:':articleId',
+    component: NewsArticlePage
   }
 ];
 
--- BobbyByarsWebsite/src/app/events/events.page.ts
+++ BobbyByarsWebsite/src/app/events/events.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Article } from '../models/article';
+//import { Firebase } from '../services/firebase.service';
 
 @Component({
   selector: 'app-events',
@@ -8,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsPage implements OnInit {
 
-  constructor() { }
+  allArticles: Article[] = [
+    new Article(
+    "Test Article Title",
+    "01/01/2024",
+    "This is the content of the test article. It is meant to demonstrate how an article will be displayed on the news article page.",
+    "https://via.placeholder.com/150"
+    ),
+    new Article(
+      "Test Article number 2",
+      "01/01/2026",
+      "This is the content of the second test article. It is meant to demonstrate how an article will be displayed on the news article page.",
+      "https://via.placeholder.com/150"
+    ),
+    
+  ];
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+  navigate(nextPage: string) {
+    this.router.navigateByUrl(nextPage);
+
+  }
+
 
 }
