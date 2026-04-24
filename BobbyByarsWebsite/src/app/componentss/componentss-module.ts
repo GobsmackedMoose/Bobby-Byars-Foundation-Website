0a82118a2e54011ac4ec83d1c48abdb4dcd4b5c5
Sarah Peeralam <speeralam@gmail.com>
2026-04-23 22:19:55 -0500

    fhhhu

--- BobbyByarsWebsite/src/app/componentss/componentss-module.ts
+++ BobbyByarsWebsite/src/app/componentss/componentss-module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../components/footer/footer.component';
 import { IonicModule } from '@ionic/angular';
 
+// this is for footer. I know the name is bad but dont deete pls. 
+//meant to manage components. its a module 
 
 @NgModule({
   declarations: [FooterComponent],
